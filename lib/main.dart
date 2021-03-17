30d837c7c7ed05d12a22d6329aceba3f7b3a3c05
lbc19920615 <1585638808@qq.com>
2021-03-17 15:17:52 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,20 @@ Future<String> fetchUserOrder() {
   return Future.delayed(Duration(seconds: 4), () => 'Large Latte');
 }
 
-String createOrderMessage() {
-  var order = fetchUserOrder();
-  return 'Your order is: $order';
+Future<String> createOrderMessage() async {
+  var order = '';
+  try {
+    order = await fetchUserOrder();
+    return 'Your order is: $order';
+  } catch (e) {
+    throw e;
+  }
 }
 
 void main() {
-  print(createOrderMessage());
+  // createOrderMessage().then((value) => {print(value)});
+
+  Stream.fromFutures([createOrderMessage()]).listen((data) {
+    print(data);
+  }, onError: (e) {}, onDone: () {});
 }
